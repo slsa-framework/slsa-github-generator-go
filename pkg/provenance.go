89eb611702f58eb28afff6e1ee22147f3d9d97f4
Asra Ali <asraa@google.com>
2022-04-01 11:53:14 -0500

    use builder
    Signed-off-by: Asra Ali <asraa@google.com>

--- pkg/provenance.go
+++ pkg/provenance.go
@@ -73,7 +73,7 @@ var (
 const (
 	requestTokenEnvKey = "ACTIONS_ID_TOKEN_REQUEST_TOKEN"
 	requestURLEnvKey   = "ACTIONS_ID_TOKEN_REQUEST_URL"
-	audience           = "https://github.com/slsa-framework/slsa-github-generator-go"
+	audience           = "slsa-framework/slsa-github-generator-go/builder"
 )
 
 type (
