423a56000a978e12b775588d05aeb3c6113eb785
laurentsimon <laurentsimon@google.com>
2022-04-27 17:29:23 +0000

    updates

--- pkg/build_test.go
+++ pkg/build_test.go
@@ -65,46 +65,6 @@ func Test_isAllowedEnvVariable(t *testing.T) {
 	}
 }
 
-func Test_marshallList(t *testing.T) {
-	t.Parallel()
-
-	tests := []struct {
-		name      string
-		variables []string
-		expected  string
-	}{
-		{
-			name:      "single arg",
-			variables: []string{"--arg"},
-			expected:  "WyItLWFyZyJd",
-		},
-		{
-			name: "list args",
-			variables: []string{
-				"/usr/lib/google-golang/bin/go",
-				"build", "-mod=vendor", "-trimpath",
-				"-tags=netgo",
-				"-ldflags=-X main.gitVersion=v1.2.3 -X main.gitSomething=somthg",
-			},
-			expected: "WyIvdXNyL2xpYi9nb29nbGUtZ29sYW5nL2Jpbi9nbyIsImJ1aWxkIiwiLW1vZD12ZW5kb3IiLCItdHJpbXBhdGgiLCItdGFncz1uZXRnbyIsIi1sZGZsYWdzPS1YIG1haW4uZ2l0VmVyc2lvbj12MS4yLjMgLVggbWFpbi5naXRTb21ldGhpbmc9c29tdGhnIl0=",
-		},
-	}
-	for _, tt := range tests {
-		tt := tt // Re-initializing variable so it is not changed while executing the closure below
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
-			r, err := marshallList(tt.variables)
-			if err != nil {
-				t.Errorf("marshallList: %v", err)
-			}
-			if !cmp.Equal(r, tt.expected) {
-				t.Errorf(cmp.Diff(r, tt.expected))
-			}
-		})
-	}
-}
-
 func Test_isAllowedArg(t *testing.T) {
 	t.Parallel()
 
